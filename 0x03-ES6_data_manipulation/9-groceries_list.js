7264c066509c3363be6926e6d44169fddebbb3fd
SoufianKm <soufian.kamma@gmail.com>
2024-07-03 01:43:40 +0000

    Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function grocerieList() {
+  const grocerieList = new Map();
+  grocerieList.set('Apples', 10);
+  grocerieList.set('Tomatoes', 10);
+  grocerieList.set('Pasta', 1);
+  grocerieList.set('Rice', 1);
+  grocerieList.set('Banana', 5);
+  return grocerieList;
+}
